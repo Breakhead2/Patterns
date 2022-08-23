7aa9683a3748c48915424e443f6834331cc30e17
Denis Sazonov <greenfish@list.ru>
2022-08-23 11:08:55 +0400

    add strategy

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Observer\{Resource, Applicant};
+use Strategy\{Shop, YandexPayment, QiwiPayment, WebMoneyPayment};
 
 include "Autoload.php";
 spl_autoload_register([new Autoload(), 'loadClass']);
@@ -8,5 +9,9 @@ spl_autoload_register([new Autoload(), 'loadClass']);
 $resource = new Resource();
 $applicant = new Applicant("Denis", "test@test.test", 2);
 
-$resource->attach($applicant);
-$resource->createVacancy("новая");
+//$resource->attach($applicant);
+//$resource->createVacancy("новая");
+
+//========================================================================================
+
+(new Shop())->choosePayment(new YandexPayment(),1200, "222-55-33");
